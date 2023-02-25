262372e7a4df3a00c569009d0afa4819d356c18a
laura <laurajorques2003@gmail.com>
2023-02-25 13:00:54 +0100

    puntos

--- Assets/Scrcipts/Block.cs
+++ Assets/Scrcipts/Block.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] private float cantidad;
+    [SerializeField] private Puntos puntos;
     void OnCollisionEnter2D(Collision2D collisionInfo) {
+        puntos.SumarPuntos(cantidad);
         Destroy(gameObject);
     }
 }
