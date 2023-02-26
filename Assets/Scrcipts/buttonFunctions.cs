702bde4737b3abcc4010263372c41106da38e359
laura <laurajorques2003@gmail.com>
2023-02-26 21:00:46 +0100

    acabado

--- Assets/Scrcipts/buttonFunctions.cs
+++ Assets/Scrcipts/buttonFunctions.cs
@@ -7,16 +7,19 @@ public class buttonFunctions : MonoBehaviour
 {
     public void Nivel1()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("nivel1");
     }
 
     public void Nivel2()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("nivel2");
     }
 
     public void Nivel3()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("nivel3");
     }
 
--- Assets/Scrcipts/menuPause.cs
+++ Assets/Scrcipts/menuPause.cs
@@ -7,17 +7,20 @@ public class menuPause : MonoBehaviour
 {
     [SerializeField] private GameObject botonPausa;
     [SerializeField] private GameObject menuPausa;
+    [SerializeField] private GameObject menuGanar;
     public void Pausa()
     {
         Time.timeScale = 0f;
         botonPausa.SetActive(false);
         menuPausa.SetActive(true);
+        menuGanar.SetActive(false);
     }
     public void Reanudar()
     {
         Time.timeScale = 1f;
         botonPausa.SetActive(true);
         menuPausa.SetActive(false);
+        menuGanar.SetActive(false);
     }
     public void RestartN1()
     {
@@ -25,6 +28,7 @@ public class menuPause : MonoBehaviour
         SceneManager.LoadScene("nivel1");
         botonPausa.SetActive(true);
         menuPausa.SetActive(false);
+        menuGanar.SetActive(false);
     }
     public void RestartN2()
     {
@@ -32,11 +36,13 @@ public class menuPause : MonoBehaviour
         SceneManager.LoadScene("nivel2");
         botonPausa.SetActive(true);
         menuPausa.SetActive(false);
+        menuGanar.SetActive(false);
     }
     public void Nivel2()
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene("nivel2");
+        menuGanar.SetActive(false);
     }
     public void RestartN3()
     {
@@ -44,11 +50,13 @@ public class menuPause : MonoBehaviour
         SceneManager.LoadScene("nivel3");
         botonPausa.SetActive(true);
         menuPausa.SetActive(false);
+        menuGanar.SetActive(false);
     }
     public void Nivel3()
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene("nivel3");
+        menuGanar.SetActive(false);
     }
     public void Home()
     {
@@ -56,5 +64,6 @@ public class menuPause : MonoBehaviour
         SceneManager.LoadScene("inicio");
         botonPausa.SetActive(true);
         menuPausa.SetActive(false);
+        menuGanar.SetActive(false);
     }
 }
